a1c920523b9502f0668208a3ef7e85a9f44a8bee
Sébastien <contact@wouaf.it>
2016-05-19 17:23:01 +0200

    Add OpenGraph meta data

--- src/php/index.php
+++ src/php/index.php
@@ -29,73 +29,134 @@ if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) &&
 	exit;
 }
 
-$content = '';
+$data = array(
+    'content'   => '',
+    'canonical' => 'https://'.$_SERVER['HTTP_HOST'].'/',
+    'head'      => '',
+);
 if (!$requestURI || $requestURI === '/') {
-    return $content;
+    return $data;
 }
 if (strpos($requestURI, '/about/') !== false) {
-    $content .= file_get_contents(__DIR__.'/../parts/about.html');
+    $data['content'] .= file_get_contents(__DIR__.'/../parts/about.html');
 }
 
 define('API_KEY', '<%= htmlWebpackPlugin.options.data.API_KEY %>');
 if ($wouafId || $userId) {
-    $content .= '<script>window.wouafit = {};</script>';
+    $data['content'] .= '<script>window.wouafit = {};</script>';
 }
 if ($wouafId) {
     try {
         //Get wouaf data from API
-        $data = curlGet(
+        $wouafData = curlGet(
             'https://api.wouaf.it/wouafs/'.$wouafId,
             null,
             array(
-                CURLOPT_HTTPHEADER => array('x-wouafit-api-key: '.API_KEY)
+                CURLOPT_HTTPHEADER => array('Authorization: WouafIt version="1", key="'.API_KEY.'"')
             )
         );
-        if ($data) {
-            $data = json_decode($data, true);
-            if ($data['code'] === 200) {
-                $content .= '<script>window.wouafit.wouaf = '.json_encode($data['wouaf']).';</script>';
+        if ($wouafData) {
+            $wouafData = json_decode($wouafData, true);
+            if ($wouafData['code'] === 200) {
+                $data['canonical'] = 'https://'.$_SERVER['HTTP_HOST'].'/wouaf/'.$wouafId.'/';
+                $data['content'] .= '<script>window.wouafit.wouaf = '.json_encode($wouafData['wouaf']).';</script>';
+                $data['head'] = getWouafOpenGraph($wouafData['wouaf']);
             }
         }
     } catch (Exception $e) {}
-
-    /*Debugger : https://developers.facebook.com/tools/debug/
-     * <meta property="og:title" content="The Rock"/>
-<meta property="og:type" content="movie"/>
-<meta property="og:url" content="http://www.imdb.com/title/tt0117500/"/>
-<meta property="og:image" content="http://ia.media-imdb.com/rock.jpg"/>
-<meta property="og:site_name" content="IMDb"/>
-<meta property="fb:admins" content="USER_ID"/>
-<meta property="og:description"
-      content="A group of U.S. Marines, under command of
-               a renegade general, take over Alcatraz and
-               threaten San Francisco Bay with biological
-               weapons."/>
-     * */
-}
-if ($userId) {
+} else if ($userId) {
     try {
         //Get user data from API
-        $data = curlGet(
+        $userData = curlGet(
             'https://api.wouaf.it/users/'.$userId,
             null,
             array(
-                CURLOPT_HTTPHEADER 	=> array('x-wouafit-api-key: ' . API_KEY)
+                CURLOPT_HTTPHEADER 	=> array('Authorization: WouafIt version="1", key="'.API_KEY.'"')
             )
         );
-        if ($data) {
-            $data = json_decode($data, true);
-            if ($data['code'] === 200) {
-                $content .= '<script>window.wouafit.user = '.json_encode($data['user']).';</script>';
+        if ($userData) {
+            $userData = json_decode($userData, true);
+            if ($userData['code'] === 200) {
+                $data['canonical'] = 'https://'.$_SERVER['HTTP_HOST'].'/user/'.$userId.'/';
+                $data['content'] .= '<script>window.wouafit.user = '.json_encode($userData['user']).';</script>';
+                $data['head'] = getUserOpenGraph($userData['user']);
             }
         }
     } catch (Exception $e) {}
 }
 
-return $content;
+return $data;
 
 /**
- * Send a GET requst using cURL
+ * Generate OpenGraph meta tags for a given Wouaf
+ * @param array $data wouaf data
+ * @return string
+ */
+function getWouafOpenGraph ($data) {
+
+    if (!empty($data['title'])) {
+        $title = $data['title'];
+    } else {
+        $title = strip_tags($data['text']);
+        $title = mb_substr($title, 0, 79). (mb_strlen($title) > 79 ? '…' : '');
+    }
+    $description = strip_tags($data['text']);
+    $description = mb_substr($description, 0, 299).(mb_strlen($description) > 299 ? '…' : '');
+    $locale = substr($_SERVER['HTTP_HOST'], 0, 5) === 'fr-fr' ? 'fr_FR' : 'en_US';
+
+    $return = '<meta property="og:title" content="'.htmlspecialchars($title).'" />'."\n".
+    '<meta property="og:type" content="article" />'."\n".
+
+    '<meta property="og:article:published_time" content="'.date('c', $data['date'][0]['sec']).'" />'."\n".
+    '<meta property="og:article:expiration_time" content="'.date('c', $data['date'][1]['sec']).'" />'."\n".
+    '<meta property="og:article:author" content="https://wouaf.it/user/'.htmlspecialchars($data['author'][1]).'/" />'."\n".
+
+    '<meta property="og:url" content="https://wouaf.it/wouaf/'.$data['id'].'/" />'."\n".
+    '<meta property="og:site_name" content="Wouaf IT" />'."\n".
+    '<meta property="og:locale" content="'.$locale.'" />'."\n".
+    '<meta property="og:description" content="'.htmlspecialchars($description).'" />'."\n";
+
+    if (!empty($data['pics']) && is_array($data['pics'])) {
+        foreach ($data['pics'] as $pic) {
+            $return .= '<meta property="og:image" content="'.htmlspecialchars($pic).'" />'."\n";
+        }
+    } else {
+        $return .= '<meta property="og:image" content="https://img.wouaf.it/icon.png" />'."\n";
+    }
+
+    return $return;
+}
+
+/**
+ * Generate OpenGraph meta tags for a given User
+ * @param array $data user data
+ * @return string
+ */
+function getUserOpenGraph ($data) {
+    $title = trim(!empty($data['firstname']) && !empty($data['lastname']) ? $data['firstname'] .' '. $data['lastname'] : $data['username']);
+    $return = '<meta property="og:title" content="'.htmlspecialchars($title).'" />'."\n".
+              '<meta property="og:type" content="profile" />'."\n".
+              '<meta property="og:url" content="https://wouaf.it/user/'.$data['username'].'/" />'."\n".
+              '<meta property="og:site_name" content="Wouaf IT" />'."\n".
+              '<meta property="og:locale" content="'.$data['lang'].'" />'."\n".
+              '<meta property="og:image" content="https://img.wouaf.it/icon.png" />'."\n";
+    if (!empty($data['description'])) {
+        $return .= '<meta property="og:description" content="'.htmlspecialchars(mb_substr(strip_tags($data['description']), 0, 300)).'" />'."\n";
+    }
+    if (!empty($data['gender'])) {
+        $return .= '<meta property="og:profile:gender" content="'.htmlspecialchars($data['gender']).'" />'."\n";
+    }
+    if (!empty($data['lastname'])) {
+        $return .= '<meta property="og:profile:last_name" content="'.htmlspecialchars($data['lastname']).'" />'."\n";
+    }
+    if (!empty($data['firstname'])) {
+        $return .= '<meta property="og:profile:first_name" content="'.htmlspecialchars($data['firstname']).'" />'."\n";
+    }
+    return $return;
+}
+
+/**
+ * Send a GET request using cURL
  * @param string $url to request
  * @param array $get values to send
  * @param array $options for cURL
