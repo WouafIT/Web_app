8b8abee4eae8c6bfd309355354804717397c42fe
Sébastien Pauchet <stoto98@yahoo.fr>
2016-09-11 11:39:07 +0200

    Update comments

--- src/assets-local/service-worker.js
+++ src/assets-local/service-worker.js
@@ -1,4 +1,4 @@
 /*
- This service worker doesn't actually do anything!
+ This service worker doesn't actually do anything but is needed by Chrome mobile to activate webapp installation
  Will add support for Offline connection and the ability to handle push messages.
  */
