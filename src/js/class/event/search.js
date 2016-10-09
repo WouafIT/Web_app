9bcb0943b7358a821d34f47545ecc8e5f17cf889
Sébastien Pauchet <stoto98@yahoo.fr>
2016-10-09 14:27:08 +0200

    Set max results to 1000

--- src/js/class/event/search.js
+++ src/js/class/event/search.js
@@ -51,7 +51,7 @@ module.exports = (function() {
 			previousSearchCount = count;
 			var unit 			= data.getString('unit');
 			var radius 			= unit === 'km' ? data.getInt('radius') : mlRadius[data.getInt('radius')];
-			if (count === 500) {
+			if (count === 1000) {
 				toast.show(i18n.t('{{max}} within {{radius}}{{unit}} (maximum reached)', {max: notificationLabel, radius: radius, unit: i18n.t(unit) }), 4000);
 			} else if (count) {
 				toast.show(i18n.t('{{wouaf}} within {{radius}}{{unit}}', { count: count, wouaf: notificationLabel, radius: radius, unit: i18n.t(unit) }), 4000);
