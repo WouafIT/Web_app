238208a04db1b44a2335dc8193d935b1483b8ddb
Sébastien <contact@wouaf.it>
2016-12-13 18:15:47 +0100

    Add color on categories titles

--- src/js/class/ui/wouaf.js
+++ src/js/class/ui/wouaf.js
@@ -152,7 +152,7 @@ module.exports = (function() {
 				(collapse
 					? ' data-toggle="collapse" data-parent=".w-accordion" data-target="#collapse-'+ obj.id +'" class="panel-title collapsed w-title '+ obj.state +'"'
 					: ' class="w-title '+ obj.state +'"'),
-				' style="border-color: ', categories.getColor(obj.cat) ,';">',
+				' style="border-color:', categories.getColor(obj.cat) ,';">',
 				'<div class="w-cat w-cat'+ obj.cat +'"', locale ,'>',
 					utils.escapeHtml(title),
 				'</div>',
@@ -160,7 +160,7 @@ module.exports = (function() {
 					'<div class="w-comments">', (obj.pics && obj.pics.length ? '<i class="fa fa-picture-o"></i> ' : ''),
 					'<a href="', url.getAbsoluteURLForStates([{name: 'wouaf', value: obj.id}, {name: 'windows', value: 'comments'}]) ,
 						'" data-action="comments" data-menu="wouaf"><i class="fa fa-comment"></i> ', utils.round(obj.com) ,'</a></div>',
-					'<span>' , categories.getLabel(obj.cat) , '</span><br />', dateLabel ,
+					'<span style="color:', categories.getDarkColor(obj.cat) ,'">' , categories.getLabel(obj.cat) , '</span><br />', dateLabel ,
 				'</div>',
 			'</div>'
 		].join('');
